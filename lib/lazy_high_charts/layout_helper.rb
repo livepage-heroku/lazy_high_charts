173f273504b237040c5ad80cbee24f1681f82496
Stephen Baldwin <stephen@envision.io>
2012-05-20 15:24:21 -0400

    wrapper option

--- lib/lazy_high_charts/layout_helper.rb
+++ lib/lazy_high_charts/layout_helper.rb
@@ -2,27 +2,28 @@
 module LazyHighCharts
   module LayoutHelper
 
-    def high_chart(placeholder, object  , &block)
+    def high_chart(placeholder, object, use_wrapper=true, &block)
       object.html_options.merge!({:id=>placeholder})
       object.options[:chart][:renderTo] = placeholder
-      high_graph(placeholder,object , &block).concat(content_tag("div","", object.html_options))
+      graph = high_graph(placeholder,object, use_wrapper, &block)
+      graph.concat(content_tag("div","", object.html_options)) if use_wrapper
     end
 
-    def high_stock(placeholder, object  , &block)
+    def high_stock(placeholder, object, &block)
       object.html_options.merge!({:id=>placeholder})
       object.options[:chart][:renderTo] = placeholder
       high_graph_stock(placeholder,object , &block).concat(content_tag("div","", object.html_options))
     end
 
-    def high_graph(placeholder, object, &block)
-      build_html_output("Chart", placeholder, object, &block)
+    def high_graph(placeholder, object, use_wrapper=true, &block)
+      build_html_output("Chart", placeholder, object, use_wrapper, &block)
     end
 
     def high_graph_stock(placeholder, object, &block)
       build_html_output("StockChart", placeholder, object, &block)
     end
 
-    def build_html_output(type, placeholder, object, &block)
+    def build_html_output(type, placeholder, object, use_wrapper=true, &block)
       options_collection = []
       object.options.keys.each do |key|
         k = key.to_s.camelize.gsub!(/\b\w/) { $&.downcase }
@@ -31,19 +32,25 @@ module LazyHighCharts
       options_collection << "series: #{object.data.to_json}"
 
       graph =<<-EOJS
-      <script type="text/javascript">
-      (function() {
-        var onload = window.onload;
-        window.onload = function(){
-          if (typeof onload == "function") onload();
           var options, chart;
           options = { #{options_collection.join(",")} };
           #{capture(&block) if block_given?}
           chart = new Highcharts.#{type}(options);
-        };
-      })()
-      </script>
       EOJS
+
+      if use_wrapper
+        graph =<<-EOJS
+        <script type="text/javascript">
+        (function() {
+          var onload = window.onload;
+          window.onload = function(){
+            if (typeof onload == "function") onload();
+            #{graph}
+          };
+        })()
+        </script>
+        EOJS
+      end
 
       if defined?(raw)
         return raw(graph) 
